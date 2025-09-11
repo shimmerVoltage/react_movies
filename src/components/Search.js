1638ad282757f3c5f35a5474428c86b5e5932cba
shimmerVoltage <shimmerVoltage@SHIMMERVOLTAGE>
2025-09-11 21:57:46 +0300

    Pagination DONE

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,22 +4,50 @@ import './Search.css';
 class Search extends React.Component {
     state =
         {
-            search: ""
+            search: "",
+            type: "all",
+            page: 1
         }
 
+    prevPage = () => {
+        this.setState
+            (
+                () => (this.state.page > 1 ? { page: this.state.page - 1 } : { page: 1 }),
+                () => (this.props.searchMovie(this.state.search, this.state.type, this.state.page))
+            )
+    }
+    nextPage = () => {
+        this.setState
+            (
+                () => ({ page: this.state.page + 1 }),
+                () => (this.props.searchMovie(this.state.search, this.state.type, this.state.page))
+            )
+    }
     handleKey = (event) => {
         if (event.key === 'Enter') this.props.searchMovie(this.state.search, this.state.type);
     }
-    handlerFilter = (event) =>
-    {
+    handlerFilter = (event) => {
         this.setState
-        (
-            () => ({type:event.target.dataset.type}),
-            () => {this.props.searchMovie(this.state.search, this.state.type);}
-        );
+            (
+                () => ({ type: event.target.dataset.type }),
+                () => { this.props.searchMovie(this.state.search, this.state.type); }
+            );
     }
+    setPage = (num) => {
+        this.setState
+            (
+                () => ({ page: num }),
+                () => { this.props.searchMovie(this.state.search, this.state.type, this.state.page); }
 
+            )
+    }
     render() {
+        let limit = 10;
+        let totalPages = Math.ceil(this.props.totalCount / limit);
+        const lastIndex = totalPages <= 10 ? totalPages + 1 : this.state.page + limit;
+        const firstIndex = totalPages <= 10 ? lastIndex - limit + lastIndex + 1 : lastIndex - limit;
+        let num = [];
+        for (let i = 0; i < totalPages + 1; i++)num.push(i);
         return (
             <>
                 <div className='search'>
@@ -36,17 +64,37 @@ class Search extends React.Component {
                 </div>
                 <div className='radio'>
                     <div>
-                        <input type="radio" name="type" data-type='all'      checked={this.state.type === 'all'}     onChange={this.handlerFilter}/><span>All</span>
+                        <input type="radio" name="type" data-type='all' checked={this.state.type === 'all'} onChange={this.handlerFilter} /><span>All</span>
                     </div>
                     <div>
-                        <input type="radio" name="type" data-type='movie'    checked={this.state.type === 'movie'}   onChange={this.handlerFilter}/><span>Movies</span>
+                        <input type="radio" name="type" data-type='movie' checked={this.state.type === 'movie'} onChange={this.handlerFilter} /><span>Movies</span>
                     </div>
                     <div>
-                        <input type="radio" name="type" data-type='series'   checked={this.state.type === 'series'}  onChange={this.handlerFilter}/><span>Series</span>
+                        <input type="radio" name="type" data-type='series' checked={this.state.type === 'series'} onChange={this.handlerFilter} /><span>Series</span>
                     </div>
                     <div>
-                        <input type="radio" name="type" data-type='game'     checked={this.state.type === 'game'}    onChange={this.handlerFilter}/><span>Games</span>
+                        <input type="radio" name="type" data-type='game' checked={this.state.type === 'game'} onChange={this.handlerFilter} /><span>Games</span>
                     </div>
+                </div>
+                <div className='navigation'>
+                    <button className='btn' onClick={this.prevPage}>Previous</button>
+                    <div>
+                        {
+                            num.slice(firstIndex, lastIndex).map
+                                (
+                                    (el, index) =>
+                                    (
+                                        <button className='btn'
+                                            key={index}
+                                            onClick={() => (this.setPage(el))}
+                                            style={{ background: this.state.page !== el ? "grey" : "" }}>
+                                            {el}
+                                        </button>
+                                    )
+                                )
+                        }
+                    </div>
+                    <button className='btn' onClick={this.nextPage}>Next</button>
                 </div>
             </>
         )
--- src/layout/Main.js
+++ src/layout/Main.js
@@ -9,28 +9,41 @@ class Main extends React.Component {
         movies: [],
         gridView: true,
         loading: false,
-        type:"all"
+        type:"all",
+        count: 0
     }
-
+    
     layoutViewSw = () => {
         this.setState(prevState => ({ gridView: !prevState.gridView }));
     }
 
     componentDidMount() {
-        fetch('https://www.omdbapi.com/?apikey=58f24cab&s=Terminator')
-            .then(response => response.json())
-            .then(data => this.setState({ movies: data.Search }))
-    }
-    searchMovie = (str, type='all') => {
+        // fetch('https://www.omdbapi.com/?apikey=58f24cab&s=Terminator')
+        //     .then(response => response.json())
+        //     .then(data => this.setState({ movies: data.Search }))
         this.setState({ loading: true })
-        fetch(`https://www.omdbapi.com/?apikey=58f24cab&s=${str.trim()}${type !== 'all' ? `&type=${type}` : ''}`)
+        fetch(`https://www.omdbapi.com/?apikey=58f24cab&s=Matrix`)
             .then(response => response.json())
             .then
             (
                 data => 
                 {
-                    if(data.Response === "True")this.setState({ movies: data.Search, loading:false });
-                    else this.setState({movies:[], loading:false});
+                    if(data.Response === "True")this.setState({ movies: data.Search, loading:false, count:data.totalResults});
+                    else this.setState({movies:[], loading:false, count:data.totalResults});
+                }
+            )
+    }
+    searchMovie = (str, type='all', page) => 
+        {
+        this.setState({ loading: true })
+        fetch(`https://www.omdbapi.com/?apikey=58f24cab&s=${str.trim()}${type !== 'all' ? `&type=${type}` : ''}${`&page=${page}`}`)
+            .then(response => response.json())
+            .then
+            (
+                data => 
+                {
+                    if(data.Response === "True")this.setState({ movies: data.Search, loading:false, count:data.totalResults});
+                    else this.setState({movies:[], loading:false, count:data.totalResults});
                 }
             )
             //this.setState({loading:false});
@@ -42,7 +55,7 @@ class Main extends React.Component {
                     <button onClick={this.layoutViewSw}>
                         {this.state.gridView ? 'Спииисок' : 'Плииитка'}
                     </button>
-                    <Search searchMovie={this.searchMovie} />
+                    <Search searchMovie={this.searchMovie} totalCount={this.state.count} />
                     {
                         !this.state.loading && 
                         this.state.movies.length ? <MovieList movies={this.state.movies} gridView={this.state.gridView}/> : <Preloader />
